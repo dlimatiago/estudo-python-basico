48209f009c0340244f7150dc660cfe48842349eb
Tiago Lima <tiaguinho.alima@gmail.com>
2020-06-11 19:17:17 -0300

    Adicionando mais uma função no módulo. A função recebe um valor e devolve formatada EX: R$1.500,50

---
+++ curso-aula_em_video/Ex 108 - Formatando Moedas em Python.py
@@ -0,0 +1,15 @@
+from modulos import moeda as m
+
+money = float(input('Digite um valor: R$ '))
+pctg = float(input('Digite uma porcentagem: '))
+form = m.moeda(money)
+rising = m.aumentar(money, pctg)
+down = m.diminuir(money, pctg)
+d = m.dobro(money)
+met = m.metade(money)
+print('-=-' * 30)
+print(f'Você informou o valor de {form} e:')
+print(f'    Com aumento de {pctg}%, {form} passa a valer {m.moeda(rising)}')
+print(f'    Com a subtração de {pctg}%, {form} passa a valer {m.moeda(down)}')
+print(f'    Temos também que o dobro de {form} é {m.moeda(d)}')
+print(f'    E sua metade é {m.moeda(met)}')
--- modulos/moeda.py
+++ modulos/moeda.py
@@ -16,3 +16,53 @@ def dobro(value=0):
 def metade(value=0):
     op = value / 2
     return op
+
+
+def moeda(value=0):
+    global converted
+    global integer
+    global decimal
+
+    coin, count = str(value), 0
+    if '.' in coin:
+        coin = coin.replace('.', ',')
+
+    for num in coin:
+        if num.isnumeric():
+            count += 1
+        else:
+            break
+    if ',' in coin:
+        integer, decimal = coin.split(',')
+    else:
+        integer, decimal = coin, '00'
+
+    if count > 3:
+        integer = list(integer)
+        i = len(integer)
+        for it in range(i, -1, -3):
+            if it < i:
+                if it != 0:
+                    integer.insert(it, '.')
+        change = ''
+        for item in integer:
+            change += item
+        integer = change
+        if len(decimal) > 2:
+            decimal = float(decimal) /10 ** len(decimal)
+            decimal = round(decimal, 2)
+            decimal = str(decimal).split('.')
+            decimal = decimal[1]
+        elif len(decimal) == 1:
+            decimal += '0'
+    else:
+        if len(decimal) > 2:
+            decimal = float(decimal) / 10 ** len(decimal)
+            decimal = round(decimal, 2)
+            decimal = str(decimal).split('.')
+            decimal = decimal[1]
+        elif len(decimal) == 1:
+            decimal += '0'
+
+    converted = 'R$ ' + integer + ',' + decimal
+    return converted
