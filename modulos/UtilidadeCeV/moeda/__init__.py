93e8413ae3039f8a57d3d9fb823b7a118738f6a3
Tiago Lima <tiaguinho.alima@gmail.com>
2020-06-11 23:18:24 -0300

    Pacote com os módulos do curso feito por mim

--- modulos/UtilidadeCeV/moeda/__init__.py
+++ modulos/UtilidadeCeV/moeda/__init__.py
@@ -1,20 +1,28 @@
-def aumentar(value=0, por=0):
+def aumentar(value=0, por=0, formatting=True):
     value *= ((por+100)/100)
+    if formatting is True:
+        value = moeda(value)
     return value
 
 
-def diminuir(value=0, por=0):
+def diminuir(value=0, por=0, formatting=True):
     value *= ((100 - por) / 100)
+    if formatting is True:
+        value = moeda(value)
     return value
 
 
-def dobro(value=0):
+def dobro(value=0, formatting=True):
     op = value * 2
+    if formatting is True:
+        op = moeda(op)
     return op
 
 
-def metade(value=0):
+def metade(value=0, formatting=True):
     op = value / 2
+    if formatting is True:
+        op = moeda(op)
     return op
 
 
@@ -40,8 +48,21 @@ def moeda(value=0, cifra='R$'):
     if len(decimal) > 2:
         decimal = round(float(decimal) / 10 ** len(decimal), 2)
         decimal = str(decimal)[2:]
-    elif len(decimal) == 1:
+    if len(decimal) == 1:
         decimal += '0'
 # Juntando as partes formatadas
     converted = cifra + integer + ',' + decimal
     return converted
+
+
+def resumo(value=0, raise_pctg=10, reduce_pctg=5):
+    print('-'*40)
+    print('ANALISE DO VALOR'.center(40))
+    print('-'*40)
+
+    print(f'Preço analisado:    \t{moeda(value)}')
+    print(f'Dobro do preço:     \t{dobro(value)}')
+    print(f'Metade do preço:    \t{metade(value)}')
+    print(f'{raise_pctg}% de aumento:       \t{aumentar(value, raise_pctg)}')
+    print(f'{reduce_pctg}% de redução:      \t{diminuir(value, reduce_pctg)}')
+    print('-'*40)
