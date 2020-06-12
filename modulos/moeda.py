d589236510fc981ce1e7fbe5d245b50fd5ab8088
Tiago Lima <tiaguinho.alima@gmail.com>
2020-06-11 21:04:26 -0300

    Melhorando e otimizando a função moeda

--- modulos/moeda.py
+++ modulos/moeda.py
@@ -18,51 +18,30 @@ def metade(value=0):
     return op
 
 
-def moeda(value=0):
+def moeda(value=0, cifra='R$'):
     global converted
     global integer
     global decimal
 
-    coin, count = str(value), 0
+    coin = str(value)
     if '.' in coin:
-        coin = coin.replace('.', ',')
-
-    for num in coin:
-        if num.isnumeric():
-            count += 1
-        else:
-            break
-    if ',' in coin:
-        integer, decimal = coin.split(',')
+        integer, decimal = coin.split('.')
     else:
         integer, decimal = coin, '00'
-
-    if count > 3:
-        integer = list(integer)
-        i = len(integer)
-        for it in range(i, -1, -3):
-            if it < i:
-                if it != 0:
-                    integer.insert(it, '.')
-        change = ''
-        for item in integer:
-            change += item
-        integer = change
-        if len(decimal) > 2:
-            decimal = float(decimal) /10 ** len(decimal)
-            decimal = round(decimal, 2)
-            decimal = str(decimal).split('.')
-            decimal = decimal[1]
-        elif len(decimal) == 1:
-            decimal += '0'
-    else:
-        if len(decimal) > 2:
-            decimal = float(decimal) / 10 ** len(decimal)
-            decimal = round(decimal, 2)
-            decimal = str(decimal).split('.')
-            decimal = decimal[1]
-        elif len(decimal) == 1:
-            decimal += '0'
-
-    converted = 'R$ ' + integer + ',' + decimal
+# Formatando a parte inteira
+    integer = list(integer)
+    for i in range(len(integer), -1, -3):
+        integer.insert(i, '.') if i < len(integer) and i != 0 else '-'
+    transition = ''
+    for item in integer:
+        transition += item
+    integer = transition
+# Formatando a parte decimal
+    if len(decimal) > 2:
+        decimal = round(float(decimal) / 10 ** len(decimal), 2)
+        decimal = str(decimal)[2:]
+    elif len(decimal) == 1:
+        decimal += '0'
+# Juntando as partes formatadas
+    converted = cifra + integer + ',' + decimal
     return converted
